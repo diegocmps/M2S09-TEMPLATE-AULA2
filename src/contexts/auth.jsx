093f6cfb202fe3156d8441a577256b0b76a8e601
diegocmps <diegobarreiros@hotmail.com>
2024-08-02 15:55:19 -0300

    feature: contexto login

---
+++ src/contexts/auth.jsx
@@ -0,0 +1,38 @@
+// contexts/auth.jsx
+
+import { useState } from "react";
+import { createContext, useContext } from "react";
+
+export const AuthContext = createContext({
+    user: null,
+    signIn: (data) => {},
+    signOut: () => {}
+})
+
+export function AuthProvider({ children }) {
+    const [user, setUser] = useState(null)
+
+    function signIn(data) {
+        if(data.email !== "teste@lab.com.br" && data.password !== "123"){
+            throw new Error('Email/Senha inválida.')
+        }
+
+        setUser({
+            id: Date.now(),
+            first_name: 'Nicholas',
+            email: data.email,
+        })
+    }
+
+    function signOut() {
+        setUser(null)
+    }
+
+    return <AuthContext.Provider value={{ signIn, signOut, user }}>{children}</AuthContext.Provider>
+}
+
+export function useAuth() {
+    const contexto = useContext(AuthContext)
+
+    return contexto
+}
